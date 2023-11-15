ba8e1d06c71e0e37ceddb1889639b29d3301d7e9
kirti <kirti@KMK>
2023-11-15 12:50:38 -0600

    helper hooks

---
+++ src/test/java/stepdefinition/Bclass.java
@@ -0,0 +1,11 @@
+package stepdefinition;
+
+import pageobjects.Datastructure;
+import pageobjects.Login;
+
+public class Bclass {
+ public Login fromloginjava;
+ 
+ public Datastructure fromDSjava;
+
+}
